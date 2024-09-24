11fe200cd625cba21e6c4a2d92e058e1c6f2e9a0
Wouter ibens <wouter.ibens@xemex.eu>
2024-09-24 21:35:57 +0200

    Improve example

--- examples/networkmanager.cpp
+++ examples/networkmanager.cpp
@@ -11,29 +11,25 @@ int main() noexcept(false) {
     auto devices = networkManager.get<Array<ObjectPath>>("org.freedesktop.NetworkManager", "Devices");
 
     for (const auto &device : devices) {
-        std::cout << "Device:     " << device << std::endl;
         auto nmDevice = Properties{&dbus, "org.freedesktop.NetworkManager", device};
         std::cout << "Interface:  " << nmDevice.get<String>("org.freedesktop.NetworkManager.Device", "Interface")
                   << std::endl;
         std::cout << "MacAddress: " << nmDevice.get<String>("org.freedesktop.NetworkManager.Device", "HwAddress")
                   << std::endl;
-        std::cout << "Driver:     " << nmDevice.get<String>("org.freedesktop.NetworkManager.Device", "FirmwareVersion")
-                  << std::endl;
-        std::cout << "Connections:" << std::endl;
-        for (const auto [property, interface] :
-             {std::make_pair("Ip4Config", "org.freedesktop.NetworkManager.IP4Config"),
-              std::make_pair("Ip6Config", "org.freedesktop.NetworkManager.IP6Config")}) {
+        static constexpr auto properies = {std::make_pair("Ip4Config", "org.freedesktop.NetworkManager.IP4Config"),
+                                           std::make_pair("Ip6Config", "org.freedesktop.NetworkManager.IP6Config")};
+        for (const auto [property, interface] : properies) {
             auto connection = nmDevice.get<ObjectPath>("org.freedesktop.NetworkManager.Device", property);
             if (connection != "/") {
                 auto peer = Peer{&dbus, "org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager"};
                 auto v4cfg = Properties{&dbus, "org.freedesktop.NetworkManager", connection};
                 auto v4addr = v4cfg.get<Array<Dict<String, Variant<String, U32>>>>(interface, "AddressData");
                 for (const auto &addr : v4addr) {
-                    std::cout << "* Address: " << std::get<String>(addr.at("address")) << "/"
+                    std::cout << "Address:    " << std::get<String>(addr.at("address")) << "/"
                               << std::get<U32>(addr.at("prefix")) << std::endl;
                 }
             }
         }
-        std::cout << std::endl << std::endl;
+        std::cout << std::endl;
     }
 }
